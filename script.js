ac1afc1316d166abd93c6520a5cb11ab35738ebe
Anshuman <anshumanshukla.12as@gmail.com>
2024-05-31 14:15:30 +0530

    GSAP

---
+++ script.js
@@ -0,0 +1,17 @@
+gsap.to("#box",{
+    x:1000,
+    duration:2, //kitni der tak chale ga animation
+    delay:1, //Kitni der baad suru hoga animation
+    rotate:360,
+    backgroundColor:"yellow"
+    // y:1000
+}) //initial
+
+
+gsap.from("#box",{
+    x:-1000,
+    duration:2, //kitni der tak chale ga animation
+    delay:1, //Kitni der baad suru hoga animation
+    rotate:360,
+    backgroundColor:"yellow"
+})
