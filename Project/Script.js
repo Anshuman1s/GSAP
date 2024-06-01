551533d0d41ef32a5e704bb14e36a8e3e35f2bf7
Anshuman <anshumanshukla.12as@gmail.com>
2024-06-01 09:09:42 +0530

    Project

--- Project/Script.js
+++ Project/Script.js
@@ -1,7 +1,22 @@
-gsap.from("#nav h3",{
+var tl = gsap.timeline();
+tl.from("#nav h3",{
     y:-50,
     opacity:0,
     delay:0.4,
     duration:1,
     stagger:0.32,
+})
+tl.from("#image",{
+    x:-500,
+    opacity:0,
+    duration:0.5,
+
+
+
+})
+tl.from("#main h1",{
+    x:-500,
+    opacity:0,
+    duration:1,
+    stagger:0.9,
 })
