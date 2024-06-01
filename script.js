7ccf03b67ac88a1202d7ed964c69a1dc654e5a1a
Anshuman <anshumanshukla.12as@gmail.com>
2024-06-01 07:29:10 +0530

    GSAP

--- script.js
+++ script.js
@@ -11,10 +11,34 @@
 */
 
 // final to initial we will use gsap.from
+/*
 gsap.from("#box",{
     x:1000,
     duration:2, //kitni der tak chale ga animation
     delay:1, //Kitni der baad suru hoga animation
     rotate:360,
     backgroundColor:"yellow"
+})
+*/
+
+gsap.to("#box1",{
+    x:1200,
+    rotate:360,
+    scale:0.5,
+    duration:2,
+    delay:1
+})
+gsap.to("#box2",{
+    x:1200,
+    rotate:360,
+    scale:0.5,
+    duration:2,
+    delay:3
+})
+gsap.to("#box3",{
+    x:1200,
+    rotate:360,
+    scale:0.5,
+    duration:2,
+    delay:5
 })
