ed36b0561a6fb6a50b998666a72df9ee7fb00519
Anshuman <anshumanshukla.12as@gmail.com>
2024-06-02 07:26:06 +0530

    Ping

--- Concept/Ping.js
+++ Concept/Ping.js
@@ -1,11 +1,12 @@
-gsap.to("#page2 #image",{
+gsap.to("#page2 img",{
     width:"100%",
     scrollTrigger:{
         trigger:"#page2",
-        scroller:body,
+        scroller:"body",
         markers:true,
         start:"top 0",
         end:"top -100%",
         scrub:2,
+        pin:true
     }
 })
