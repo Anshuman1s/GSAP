cc3b84c4fae8b969c93d4b3b5f38e873c87a0826
Anshuman <anshumanshukla.12as@gmail.com>
2024-06-04 17:49:42 +0530

    LocoMotive

---
+++ Project5/LocoScroll.js
@@ -0,0 +1,33 @@
+gsap.from("#page1 #box",{
+    scale:0,
+    duration:1,
+    delay:1,
+    opacity:0,
+    rotate:360
+})
+gsap.from("#page2 #box",{
+    scale:0,
+    duration:1,
+    delay:1,
+    opacity:0,
+    rotate:360,
+    scrollTrigger:"#page2 #box",
+    start:"top 50%",
+    marker:true,
+})
+gsap.from("#page3 #box",{
+    scale:0,
+    duration:1,
+    delay:1,
+    opacity:0,
+    rotate:360,
+    scrollTrigger:"#page3 #box",
+    start:"top 50%",
+    marker:true,
+})
+import LocomotiveScroll from 'locomotive-scroll';
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('[data-scroll-container]'),
+    
+});
