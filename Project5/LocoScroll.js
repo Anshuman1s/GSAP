5d524e2c62861c0b1513cc49ed4f8f4b457d4f0c
Anshuman <anshumanshukla.12as@gmail.com>
2024-06-05 12:15:18 +0530

    LocoMotive

--- Project5/LocoScroll.js
+++ Project5/LocoScroll.js
@@ -1,8 +1,39 @@
 
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true
+// const scroll = new LocomotiveScroll({
+//     el: document.querySelector('#main'),
+//     smooth: true
+// });
+gsap.registerPlugin(ScrollTrigger);
+
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true
 });
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy("#main", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+});
+
+
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.refresh();
+
 gsap.from("#page1 #box",{
     scale:0,
     duration:1,
@@ -13,20 +44,22 @@ gsap.from("#page1 #box",{
 gsap.from("#page2 #box",{
     scale:0,
     duration:1,
-    delay:1,
+    delay:0.3,
     opacity:0,
     rotate:360,
     scrollTrigger:"#page2 #box",
+    scroller:"#main",
     start:"top 50%",
     marker:true,
 })
 gsap.from("#page3 #box",{
     scale:0,
     duration:1,
-    delay:1,
+    delay:0.3,
     opacity:0,
     rotate:360,
     scrollTrigger:"#page3 #box",
     start:"top 50%",
+    scroller:"#main",
     marker:true,
 })
