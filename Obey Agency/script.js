d845f9febd0f99dba6ca8fa11659fde1b0b8425a
Anshuman <anshumanshukla.12as@gmail.com>
2024-06-06 09:01:43 +0530

    Obey Agency Website

--- Obey Agency/script.js
+++ Obey Agency/script.js
@@ -1,6 +1,4 @@
-
-
-var tl = gsap.timeline()
+var tl = gsap.timeline();
 tl.from(".line h1",{
     y:150,
     stagger:0.25,
@@ -14,13 +12,13 @@ tl.from("#line1-part1, .line h2",{
 var grow = 0;
 setInterval(function(){
     
-    if(grow<=100){
+    if(grow<100){
         // grow++;
         h5timer.innerHTML = grow++;
     }else{
-        grow = 100;
+        h5timer.innerHTML = grow
     }
-},35)
+},33)
     }
 })
 
@@ -28,4 +26,9 @@ tl.to("#loader",{
     opactiy:0,
     duration:0.4,
     delay:4
+})
+tl.from("#page1",{
+    y:1200,
+    opacity:0,
+    
 })
